051dc0574b00d8bfc4a8a41e19a170401f5069ab
Chris Lin <chris4540@gmail.com>
2018-12-20 22:51:20 +0100

    Use dynamic programming

--- assign2/src/sec2_3/main.py
+++ assign2/src/sec2_3/main.py
@@ -9,13 +9,18 @@ class Tree(tree_helper.Tree):
     TODO: class documentation
     """
 
+
     def load_sample(self, sample):
+
+        self.s_values = dict()
+
         ex_2_3.load_sample(self.root, sample)
 
     def s_fun(self, node, value):
         """
         TODO: rename this function
         """
+        # print("s_fun({}, {})".format(node.name, value))
         if self.is_leaf(node):
             if node.sample == value:
                 return 1
@@ -25,12 +30,18 @@ class Tree(tree_helper.Tree):
         # for each child of this node, consider all its possible values
         ret = 1
         for c in node.descendants:
-            node_evidence = 0    # the sum of the blanket
-            for i, w in enumerate(c.cat[value][:]):
-                node_evidence += w * self.s_fun(node=c, value=i)
+            key = "s_fun({}, {})".format(node.name, value)
+            if key in self.s_values:
+                node_evidence = self.s_values[key]
+            else:
+                node_evidence = 0    # the sum of the blanket
+                for i, w in enumerate(c.cat[value][:]):
+                    node_evidence += w * self.s_fun(node=c, value=i)
+
+                # save the node_evidence
+                self.s_values[key] = node_evidence
 
             ret *= node_evidence
-
         return ret
 
     @staticmethod
@@ -54,7 +65,7 @@ class Tree(tree_helper.Tree):
         Return:
             return the probability of the set observations (samples)
         """
-
+        self.s_values = dict()
         # consider all posible value of the root
         # prob: the probability of the root has that i-th catagory
         #    i: the label of a catagory
