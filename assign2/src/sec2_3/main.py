9bcf37f54f004adfb50714562c5bdaca6462213d
Chris Lin <chris4540@gmail.com>
2019-01-06 09:34:35 +0100

    [2.3] Add methods to generate the result csv

--- assign2/src/sec2_3/main.py
+++ assign2/src/sec2_3/main.py
@@ -2,23 +2,27 @@ import numpy as np
 import pickle
 import ex_2_3
 import ex_2_3_tree_helper as tree_helper
+import pandas as pd
 
 class Tree(tree_helper.Tree):
     """
     Inherit from the tree helper
-    TODO: class documentation
     """
     st_evids = dict()  # subtree evidences dictionary
 
     def load_sample(self, sample):
+        # clean out the saved s(uv, i) results
         self.st_evids = dict()
+        # use the provided function
         ex_2_3.load_sample(self.root, sample)
 
     def s_fun(self, node, value):
         """
-        TODO: rename this function
+        The s(u, v) in mentioned in the problem
+        Args:
+            node (Node): the node
+            value (number): the value of this node
         """
-        # print("s_fun({}, {})".format(node.name, value))
         if self.is_leaf(node):
             if node.sample == value:
                 return 1
@@ -34,6 +38,7 @@ class Tree(tree_helper.Tree):
                 st_evid = self.st_evids[key]
             else:
                 st_evid = 0    # the sum of the blanket
+                # calculate the result recursively
                 for i, w in enumerate(c.cat[value][:]):
                     st_evid += w * self.s_fun(node=c, value=i)
                 # save the node_evidence
@@ -80,12 +85,26 @@ if __name__ == '__main__':
     params = np.load("./tree_params.npy").tolist()
     samples = np.load("./tree_samples.npy").tolist()
 
+    result_dict = dict()
     for param_k, param_v in params.items():
         t = Tree()
         t.load_params(param_v)
+        # for each tree, create a dictionary to save the result
+        result_dict[param_k] = dict()
         for s in range(1, 4):
+
             sample_name = "{}_sample_{}".format(param_k, s)
             t.load_sample(samples[sample_name])
+            p_obs_nodes = t.get_obs_prob()
             print(sample_name)
-            print(t.get_obs_prob())
+            print(p_obs_nodes)
+            result_dict[param_k]["sample_{}".format(s)] = p_obs_nodes
+
+    # transform the result to csv
+
+    df = pd.DataFrame.from_dict(result_dict, orient="index")
+    df.index.name = "Tree"
+    df = df[["sample_{}".format(i) for i in range(1, 4)]]
+    df.to_csv("result.csv", float_format='%.3e')
+
 
